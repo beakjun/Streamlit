1770883c5b0027e39654a08a12629f6c28fc69c1
beakjun <wjsqorwns93@naver.com>
2023-09-08 17:31:36 +0900

    postgres test 1

--- app.py
+++ app.py
@@ -1,30 +1,24 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-
-st.title('')
-
-### Postgres 연결
 import psycopg2
-
-# 데이터베이스 설정 값
-DATABASE = 'your_database_name'
-USER = 'your_user_name'
-PASSWORD = 'your_password'
-HOST = 'localhost'  # or your database host
-PORT = '5432'  # default port for PostgreSQL, change if yours is different
-
-# 데이터베이스에 연결
-connection = psycopg2.connect(
-    dbname=DATABASE,
-    user=USER,
-    password=PASSWORD,
-    host=HOST,
-    port=PORT
-)
-
-# 커서 생성
-cursor = connection.cursor()
+st.title('Postgres Connection Test')
+### Postgres 연결
 
 
+@st.cache_resource
+def init_connection():
+    return psycopg2.connect(**st.secrets["postgres"])
 
+conn = init_connection()
+
+@st.cache_data(ttl=600)
+def run_query():
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+    
+rows = run_query("SELECT * from airflow.stock_market_tbl limit 50")
+
+for row in rows:
+    st.write(f"{row[0]} has a :{row[1]}:")
