f828a9241fe588c7388df97a2278f5bedbfcb2b1
beakjun <wjsqorwns93@naver.com>
2023-09-11 13:56:03 +0900

    search box test 1

--- app.py
+++ app.py
@@ -10,17 +10,18 @@ st.title('Postgres Connection Test')
 def init_connection():
     return psycopg2.connect(**st.secrets["postgres"])
 
+
+
 conn = init_connection()
+cursor=conn.cursor()
+title = st.text_input('주식명', '삼성전자')
+
 
 @st.cache_data(ttl=600)
 def run_query(query):
-    with conn.cursor() as cur:
-        cur.execute(query)
-        return cur.fetchall()
-    
-rows = run_query("SELECT * from airflow.stock_market_tbl limit 50")
-
-for row in rows:
-    st.write(f"{row[0]} has a :{row[1]}:")
-#이제 포스트레 테스트 완료
+    df = pd.read_sql_query(query,con=conn)
+    return df
+query=f"SELECT * from airflow.stock_market_tbl where \"itmsNm\" like '%{title}%'"
+df = run_query(query)
+st.dataframe(df)
     
