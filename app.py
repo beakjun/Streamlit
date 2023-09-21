bd2ba2afc66420a9b6d914e8baa369b2a8247ac9
beakjun <wjsqorwns93@naver.com>
2023-09-21 14:41:51 +0900

    indicator class and multibox option chart update

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ import psycopg2
 import plotly.graph_objects as go
 
 import candlechart
+from TechnicalIndicators import TechnicalIndicators
+
+
+
 
 
 ### Postgres 연결
@@ -14,6 +18,8 @@ def init_connection():
     return psycopg2.connect(**st.secrets["postgres"])
 
 conn = init_connection()
+
+# 검색 셀레트 박스에 들어가는 리스트 생성
 def make_searchlist():
     query='select distinct "itmsNm","srtnCd" from airflow.stock_market_tbl order by "srtnCd"'
     df=pd.read_sql_query(query,con=conn)
@@ -21,7 +27,7 @@ def make_searchlist():
     return list(df['new'])
 
 
-
+# 검색 셀렉트 박스 & 보조지표 멀티박스
 placeholder = st.empty()
 col1, col2 = st.columns(2)
 with col1 :
@@ -36,22 +42,33 @@ with col2 :
         bojo = st.multiselect("보조 지표",["MACD","RSI","모멘텀"])
 
 
-
+### 검색 결과 데이터 프레임 생성 
 @st.cache_data(ttl=600)
 def run_query(query):
     df = pd.read_sql_query(query,con=conn)
     return df
 query2=f"SELECT * from airflow.stock_market_tbl where \"itmsNm\" = '{option[:-8]}' order by \"basDt\" "
 df = run_query(query2)
-df['basDt'] = pd.to_datetime(df['basDt'], format='%Y%m%d')
-df['fltRt']=df['fltRt'].astype(float)
-df = df.astype({'clpr':int,'vs':int,'mkp':int,'hipr':int,'lopr':int,'trqu':int,'trPrc':int,'clpr':int,'lstgStCnt':int,'mrktTotAmt':int,})
 
-st.plotly_chart(candlechart.plot_candlestick(df, title = '주식 캔들 차트'))
+### class 호출
+indicators = TechnicalIndicators(df)
+indicators.preprocess_data()
+main_df=indicators.df.copy()
 
+indicators.compute_macd()
+macd_df=indicators.df
 
-st.dataframe(df.sort_values(by='basDt', ascending = False).head(20))
+indicators.compute_rsi()
+rsi_df=indicators.df
 
+st.plotly_chart(candlechart.plot_candlestick(main_df, title = '주식 캔들 차트'))
+
+if 'MACD' in bojo:
+    st.write('Show MACD Chart')
+if 'RSI' in bojo:
+    st.write('Show RSI Chart')
+
+st.dataframe(main_df.sort_values(by='basDt', ascending = False).head(20))
 
 
 placeholder.title(f'{option}')
